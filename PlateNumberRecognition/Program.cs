e369ebc60b1ba5e8acb0bd020bcaefc25b74645d
Yurchester <peetlucifer@gmail.com>
2019-05-10 23:44:14 +0300

    Add project files.

---
+++ PlateNumberRecognition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlateNumberRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
